3ed840b566669ae1c47aa8c2c85d77d079852981
Karl Lorey <git@karllorey.com>
2020-07-31 21:23:29 +0200

    Add example for quotes.toscrape.com

---
+++ examples/quotes_to_scrape.py
@@ -0,0 +1,38 @@
+import requests
+
+from autoscraper import SingleItemSample, SingleItemScraper
+
+
+def main():
+    items = {
+        "http://quotes.toscrape.com/author/Eleanor-Roosevelt/": {
+            "name": "Eleanor Roosevelt",
+            "birth": "October 11, 1884",
+        },
+        "http://quotes.toscrape.com/author/Andre-Gide/": {
+            "name": "André Gide",
+            "birth": "November 22, 1869",
+        },
+        "http://quotes.toscrape.com/author/Thomas-A-Edison/": {
+            "name": "Thomas A. Edison",
+            "birth": "February 11, 1847",
+        },
+    }
+    results = {url: requests.get(url) for url in items.keys()}
+
+    # train scraper
+    samples = [
+        SingleItemSample(items[url], results[url].content) for url in items.keys()
+    ]
+    scraper = SingleItemScraper.build(samples)
+
+    print("Scraping Albert Einstein")
+    html = requests.get("http://quotes.toscrape.com/author/Albert-Einstein/").content
+    result = scraper.scrape(html)
+
+    print("Result: %s" % result)
+    # > Result: {'birth': 'March 14, 1879', 'name': 'Albert Einstein'}
+
+
+if __name__ == "__main__":
+    main()
