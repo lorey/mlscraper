5fc54bfb868d2f1e6f40f16cdef8a33b68fa7412
Karl Lorey <git@karllorey.com>
2020-09-27 13:21:17 +0200

    Set verion to 0.0.0 to use bumpversion

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lorey/mlscraper",
-    version="0.1.0",
+    version="0.0.0",
     zip_safe=False,
 )
