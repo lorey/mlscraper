68e59a948cd2e448bdd79240456ce447b1cf04db
Karl Lorey <git@karllorey.com>
2020-09-27 13:54:48 +0200

    Bump version: 0.1.1 → 0.1.2

--- mlscraper/__init__.py
+++ mlscraper/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Karl Lorey"""
 __email__ = "git@karllorey.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 import logging
 import random
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lorey/mlscraper",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
