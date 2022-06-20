fb0dbdc02c069fbd12fc220ff21bd04faa14b180
Karl Lorey <git@karllorey.com>
2022-06-20 21:21:29 +0200

    Move package metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,46 +1,13 @@
 #!/usr/bin/env python
 """The setup script."""
-from setuptools import find_packages
 from setuptools import setup
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
-requirements = [
-    "beautifulsoup4",
-    "lxml",
-    "more-itertools>=8",
-    "requests",
-]
-
-setup_requirements = ["pytest-runner"]
-
-test_requirements = ["pytest>=3"]
 
 setup(
-    author="Karl Lorey",
-    author_email="git@karllorey.com",
-    python_requires=">=3.9",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-    ],
-    description="Scrape HTML automatically with machine learning.",
-    install_requires=requirements,
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="mlscraper",
     name="mlscraper",
-    packages=find_packages(include=["mlscraper", "mlscraper.*"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/lorey/mlscraper",
-    version="0.1.2",
-    zip_safe=False,
+    install_requires=[
+        "beautifulsoup4",
+        "lxml",
+        "more-itertools>=8",
+    ],
 )
