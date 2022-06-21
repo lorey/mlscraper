a879b8d9fcf90e530c0356dc030a6d0cc091cc4e
Karl Lorey <git@karllorey.com>
2022-06-21 17:34:47 +0200

    Add exemplary return to quotes example

--- examples/quotes_to_scrape.py
+++ examples/quotes_to_scrape.py
@@ -29,6 +29,7 @@ def main():
     resp = requests.get('http://quotes.toscrape.com/author/J-K-Rowling')
     result = scraper.get(Page(resp.content))
     print(result)
+    # returns {'name': 'J.K. Rowling', 'born': 'July 31, 1965'}
 
 
 if __name__ == '__main__':
