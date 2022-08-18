1887459468e37942218d4be3ac1187709cb883ab
Karl Lorey <git@karllorey.com>
2022-08-18 17:38:56 +0200

    Fix testing examples by only running them in specialized environment

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,10 +1,16 @@
-from examples.quotes_to_scrape import main as quotes_to_scrape_main
+import os
+
+import pytest
+
+PYTEST_SKIP_EXAMPLES = os.environ.get("PYTEST_SKIP_EXAMPLES", "1")
 
 
+@pytest.mark.skipif(PYTEST_SKIP_EXAMPLES == "1", reason=f'"{PYTEST_SKIP_EXAMPLES=}"')
 def test_example_quotes_to_scrape():
     """
     Test if the example quotes_to_scrape.py works
     """
+    from examples.quotes_to_scrape import main as quotes_to_scrape_main
 
     assert quotes_to_scrape_main() == {
         "name": "J.K. Rowling",
